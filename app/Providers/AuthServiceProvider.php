81ca2e5e4394e58fb896080d454de145dca94e73
Jbortweb <jbortweb@gmail.com>
2023-11-03 12:53:40 +0100

    Autentificacion con breeze

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        VerifyEmail::toMailUsing(function ($notifiable,$url) {
+            return(new MailMessage)
+            ->subject("Verificar cuenta")
+            ->line("Tu cuenta ya esta casi lista, solo debes presionar al botón para finalizarla con exito")
+            ->action("Confirmar cuenta", $url)
+            ->line("Si no creaste esta cuenta puedes ignorar este mensaje");
+        });
+    }
+}

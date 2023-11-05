39716db8a0367617a7c1d7e96b6cd2f0cb798ae0
Jbortweb <jbortweb@gmail.com>
2023-11-05 10:01:56 +0100

    Creando el panel de administracion

---
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -0,0 +1,36 @@
+<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+    @forelse ($vacantes as $vacante)
+        
+    <div class="p-6 text-gray-900 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center"> 
+        <div class="space-y-3">
+            <a href="" class="text-xl font-bold">
+                {{$vacante->titulo}}
+            </a>
+            <p class="text-sm text-gray-600 font-bold">
+                {{$vacante->empresa}}
+            </p>
+            <p class="text-sm text-gray-500">
+                Último día: {{$vacante->ultimo_dia->format('d/m/y')}}
+            </p>
+        </div> 
+
+
+        <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
+            <a href="" class=" bg-slate-800 py-2 px-4 rounded-lg text-white uppercase text-xs font-bold text-center">
+                Candidatos
+            </a>
+            <a href="" class=" bg-blue-800 py-2 px-4 rounded-lg text-white uppercase text-xs font-bold text-center">
+                Editar
+            </a>
+            <a href="" class=" bg-red-600 py-2 px-4 rounded-lg text-white uppercase text-xs font-bold text-center">
+                Eliminar
+            </a>
+        </div> 
+    </div>
+    @empty
+    <p class="py-3 text-center text-sm text-gray-600">No hay ofertas que mostrar</p>
+    @endforelse
+</div>
+<div class="mt-10">
+    {{$vacantes->links()}}
+</div>
--- resources/views/vacantes/index.blade.php
+++ resources/views/vacantes/index.blade.php
@@ -7,11 +7,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Ofertas de empleo") }}
-                </div>
+
+            @if (session()->has('mensaje'))
+
+            <div class="uppercase border border-green-600 bg-green-100 text-green-700 font-bold p-2 my-3 text-sm">
+                {{session('mensaje')}}
             </div>
+                
+            @endif
+            <livewire:mostrar-vacantes/>
         </div>
     </div>
 </x-app-layout>

c9ef66b12f7dfa5ed35862c1c0b6fae02964d9e3
Jbortweb <jbortweb@gmail.com>
2023-11-08 11:08:23 +0100

    Notificaciones

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Vacante;
+use App\Notifications\NuevoCandidato;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -39,8 +40,8 @@ class PostularVacante extends Component
         ]);
         //Crear notificacion y enviar el email
 
-
-        //Mostrar el usuario un mensaje de okç
+        $this->vacante->reclutador->notify(new NuevoCandidato($this->vacante->id, $this->vacante->titulo, auth()->user()->id));
+        //Mostrar el usuario un mensaje de ok
 
         session()->flash('mensaje','Se envió correctamente tu información, mucha suerte');
         return redirect()->back();
--- app/Models/Vacante.php
+++ app/Models/Vacante.php
@@ -34,4 +34,8 @@ class Vacante extends Model
     {
         return $this->hasMany(Candidato::class);
     }
+    public function reclutador()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
